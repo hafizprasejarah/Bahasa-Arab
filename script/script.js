1ec726394d4f68e0bff546e4cb9733c4081e13ec
Hafiz Pratama <hafizpratama0809@gmail.com>
2025-11-18 08:35:30 +0700

    first

---
+++ script/script.js
@@ -0,0 +1,20 @@
+function bab(n, page) {
+    if (page === "bab") {
+        // Arahkan ke halaman bab-1, bab-2, dst
+        window.location.href = `bab${n}-menu.html`;
+
+    } else if (page === "evaluasi") {
+        // Arahkan ke halaman evaluasi
+        window.location.href = "evaluasi.html";
+
+    } else if (n && page === null) {
+        window.location.href = `ARAB/Bab ${n}.html`;
+    } else {
+        console.error("Parameter tidak valid!");
+    }
+}
+
+function babPage(page, hal) {
+    window.location.href = `bab/${page}Bab${hal}.html`
+}
+
